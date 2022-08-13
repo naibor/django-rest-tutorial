8eedc0f17c7c5ce7a56ccb2cabc0225851f4a5f5
naibor <liznaibor.en@gmail.com>
2022-08-13 12:29:55 +0300

    add snippet's app urls to root urlconf

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from snippets import views
 
 urlpatterns = [
+    path('', include('snippets.urls')),
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
