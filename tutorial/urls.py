1b89cf57e4f25e33dafbbe4f80f137c7a6a8da91
naibor <liznaibor.en@gmail.com>
2022-08-13 19:00:08 +0300

    delete misplaced files

--- tutorial/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from snippets import views
-
-urlpatterns = [
-    path('', include('snippets.urls')),
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
-]
-
