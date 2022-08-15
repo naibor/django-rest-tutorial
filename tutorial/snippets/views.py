137a2a7af7cd3172b7d167f391c64d9c0460da20
naibor <liznaibor.en@gmail.com>
2022-08-15 19:11:24 +0300

    update views to include readonly for the user

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,5 +1,5 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
 
 class SnippetList(generics.ListCreateAPIView):
@@ -10,6 +10,22 @@ class SnippetList(generics.ListCreateAPIView):
     serializer_class = SnippetSerializer
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Get, update and delete a single snippet
+    """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+class UserList(generics.ListAPIViews):
+    """
+    List all users
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    """
+    get user details
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
