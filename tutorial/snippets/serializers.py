6a54521c1b3812238275c7c5691e68893e618e5f
naibor <liznaibor.en@gmail.com>
2022-08-15 19:04:25 +0300

    add user serializer, with a reverse relationship to User model

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
-
+from django.contrib.auth.models import User
 
 class SnippetSerializer(serializers.ModelSerializer):
     """
@@ -9,3 +9,10 @@ class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.object.all())
+
+    class Meta:
+        model = User
+        fields = ['id','username', 'snippets']
