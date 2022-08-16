287ee82cdbcbdd55d69466d10c375ad42428cc9f
naibor <liznaibor.en@gmail.com>
2022-08-16 20:05:35 +0300

    fix import error

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 # for highlighting code
-from pygments.lexers import get_all_lexers
+from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles
 from pygments.formatters.html import HtmlFormatter
 from pygments import highlight
